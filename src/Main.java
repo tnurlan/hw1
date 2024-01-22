b4b209c05c4cc056dfc35407de93bd4379198b8d
Toma <230434@astanait.edu.kz>
2024-01-22 22:28:45 +0600

    first

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        Employee employee1 = new Employee("John", "Lennon", "Manager", 27045.78);
+        Employee employee2 = new Employee("George", "Harrison", "Developer", 50000.00);
+        Student student1 = new Student("Ringo", "Starr", 0.00);
+        Student student2 = new Student("Paul", "McCartney", 36660.00);
+
+        people.add(student1);
+        people.add(employee1);
+        people.add(student2);
+        people.add(employee2);
+
+
+        Collections.sort(people);
+
+        printData(people);
+    }
+
+    public static void printData(Iterable<Person> people) {
+        for (Person person : people) {
+            System.out.println(person.toString());
+        }
+    }
+}
